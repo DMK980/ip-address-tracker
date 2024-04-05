71815c5f2d5a483eb65aaa80ccee50d9bbec48ce
DMK980 <derrick.mutuma@gmail.com>
2024-04-05 22:36:01 +0100

    api call func made

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ const submitbtn = document.getElementById("submitbtn")
 const inputcontainer = document.getElementsByClassName("ip-input-container")[0]
 const error = document.getElementsByClassName("error-message")[0]
 
+const ip = "";
+const domain = "";
+
 // validation logic
 const ipAddressValidation = (input)=>{
     let ipv4 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
@@ -34,12 +37,35 @@ const ipAddressValidation = (input)=>{
         if(!error.classList.contains(errorvisible)){
             error.classList.add(errorvisible)
         }
-    }
-    
+        return false
+    }   
 }
 
+// call to the api
 
+const apiCall = async ()=>{
 
+    let apikey = "at_9gfTpPGgkIMtt8kw8dFhMIcz7IeOy";
+    let url = `https://geo.ipify.org/api/v2/country,city?apiKey=${apikey}&ipAddress=${ip}&domain=${domain}`
+
+    try{
+        const response = await fetch(url,{
+            method: "GET"
+        })
+        let data = await response.json(); 
+        console.log(data.location) 
+    } catch(error){
+        console.log(error)
+    }
+    // console.log(data.ip) 
+}
+let x = 0
+while (x<1){
+    apiCall()
+    x++
+}
+
+// Click event of the submit button
 submitbtn.addEventListener("click",()=>{
     let inputval = input.value
     ipAddressValidation(inputval)
